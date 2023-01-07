d6c6b9bf701957dfe1273b513783ef5981f545dc
Luis Hernandez <lshrnndz6@gmail.com>
2023-01-07 15:56:50 -0800

    Lab done

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,11 +1,17 @@
 import React from "react";
+import blogData from "../data/blog";
 import Article from "./Article";
 
 function ArticleList(){
+    const articleList = blogData.posts.map((data)=> {
+        return <Article
+        id={data.id}
+        title={data.title}
+        date={data.date}
+        preview={data.preview} />
+    })
     return (
-        <main>
-            <Article />
-        </main>
+        <main>{articleList}</main>
     )
 }
 
